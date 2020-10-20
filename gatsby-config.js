e3c369d6f5f3b24f829a7d8bcf9f2e8890a98e23
Chau Giang <giangc@acm.org>
2020-10-20 02:28:26 +0000

    feat(page): WIP

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,13 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-
-module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+plugins: [
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `markdown-pages`,
+      path: `${__dirname}/src/markdown-pages`,
+    },
+  },
+  `gatsby-transformer-remark`,
+]
