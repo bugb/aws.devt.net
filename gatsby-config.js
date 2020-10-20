69477e85a9d2299334a50b3a3e1d7810a0282476
Chau Giang <giangc@acm.org>
2020-10-20 03:36:26 +0000

    feat(post): config site for using markdown pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,17 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-plugins: [
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `markdown-pages`,
-      path: `${__dirname}/src/markdown-pages`,
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+	name: `markdown-pages`,
+	path: `${__dirname}/src/markdown-pages`,
+      },
     },
-  },
-  `gatsby-transformer-remark`,
-]
+    `gatsby-transformer-remark`,
+  ],
+}
+
--- src/markdown-pages/20201020-aws-update-lambda-environment-variable-hack.md
+++ src/markdown-pages/20201020-aws-update-lambda-environment-variable-hack.md
@@ -1,5 +1,5 @@
 ---
-slug: "/blog/aws-update-lambda-environment-variable-hack"
+slug: "/aws-update-lambda-environment-variable-hack"
 date: "2020-10-20"
 title: "AWS update lambda environment hack"
 ---
